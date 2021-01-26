5d820416c8bb1a50c57c097cc22fbf39954f2715
Vitaly Alekseev <v.alekseev@orbita.center>
2021-01-27 02:32:55 +0500

    реализуем функционал и проверяем

--- src/main/java/ru/geekbrains/TddService.java
+++ src/main/java/ru/geekbrains/TddService.java
@@ -7,7 +7,27 @@ package ru.geekbrains;
 public class TddService {
 
     public String replaceOccurrences(final String str, final String what, final String to, final int count) {
-
-        return str;
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        final StringBuilder result = new StringBuilder();
+        int startIndex = 0, nextIndex = 0, replaces = 0;
+        do {
+            nextIndex = str.indexOf(what, startIndex);
+            if (nextIndex > 0) {
+                result.append(str.substring(startIndex, nextIndex));
+                if (replaces++ < count) {
+                    result.append(to);
+                } else {
+                    result.append(what);
+                }
+                startIndex = nextIndex + what.length();
+            } else {
+                result.append(str.substring(startIndex));
+                startIndex = str.length();
+            }
+        } while (startIndex < str.length());
+        return result.toString();
     }
+
 }

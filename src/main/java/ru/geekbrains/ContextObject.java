fa7f279fe0edd14f94c85f47328f9cae8b239a34
Vitaly Alekseev <v.alekseev@orbita.center>
2021-02-01 23:31:13 +0500

    Исправление нарушения закона Деметры - передается объект-контекст в качестве аргумента с использованием DSL

--- src/main/java/ru/geekbrains/ContextObject.java
+++ src/main/java/ru/geekbrains/ContextObject.java
@@ -6,12 +6,43 @@ package ru.geekbrains;
  */
 public class ContextObject {
 
+    private final String key;
+
+    private final SecondObject secondObject;
+
+    private ContextObject(String key, SecondObject secondObject) {
+        this.key = key;
+        this.secondObject = secondObject;
+    }
+
     public String get() {
-        return "qqq";
+        return key;
     }
 
     public SecondObject getSecondObject() {
         return new SecondObject();
+    }
+
+    public static class Builder {
+
+        private String key;
+
+        private SecondObject secondObject;
+
+        public Builder key(String key) {
+            this.key = key;
+            return this;
+        }
+
+        public Builder secondObject(SecondObject secondObject) {
+            this.secondObject = secondObject;
+            return this;
+        }
+
+        public ContextObject build() {
+            return new ContextObject(key, secondObject);
+        }
+
     }
 
 }
--- src/test/java/ru/geekbrains/LawOfDemeterViolationTest.java
+++ src/test/java/ru/geekbrains/LawOfDemeterViolationTest.java
@@ -11,7 +11,7 @@ public class LawOfDemeterViolationTest {
 
     @Test
     public void successTest() {
-        final int result = new LawOfDemeterViolation().testingIsRequired(new ContextObject());
+        final int result = new LawOfDemeterViolation().testingIsRequired(new ContextObject.Builder().key("ddd").secondObject(new SecondObject()).build());
         Assert.assertEquals(0, result);
     }
 
